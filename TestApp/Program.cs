9726c32c90923322e8bc6196b1f4f967e1147269
BeautySilver <32017851+BeautySilver@users.noreply.github.com>
2020-08-05 13:14:22 +0300

    Initial Commit

---
+++ TestApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+ 
+                string[] Weekends = new string[2];
+                WorkDayCalculator calculator = new WorkDayCalculator();
+
+                Console.Write("Start date:");
+                DateTime Date = Convert.ToDateTime(DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture));
+
+                Console.Write("Duration, day(s):");
+                uint Duration = UInt32.Parse(Console.ReadLine());
+
+                Console.Write("Weekends 1:");
+                string weekends = Console.ReadLine();
+                if (weekends != "0")
+                {
+                    Weekends = weekends.Split('-');
+                }
+                DateTime startWeek = Convert.ToDateTime(DateTime.ParseExact(Weekends[0], "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture));
+                DateTime endtWeek = Convert.ToDateTime(DateTime.ParseExact(Weekends[1], "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture));
+                WeekEnd[] weekEnd = new WeekEnd[]
+                {
+                    new WeekEnd(startWeek, endtWeek)
+                };
+
+                Console.Write("Weekends 2:");
+                weekends = Console.ReadLine();
+                if (weekends != "0")
+                {
+                    Weekends = weekends.Split('-');
+                    startWeek = Convert.ToDateTime(DateTime.ParseExact(Weekends[0], "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture));
+                    endtWeek = Convert.ToDateTime(DateTime.ParseExact(Weekends[1], "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture));
+                    Array.Resize(ref weekEnd, 2);
+                    weekEnd[1] = new WeekEnd(startWeek, endtWeek);
+                }
+                int Duration2 = Convert.ToInt32(Duration);
+                Console.WriteLine($"At the exit there should be: {calculator.Calculate(Date, Duration2, weekEnd).ToShortDateString()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ TestApp/WorkDayCalculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            var weekEndList = new List<DateTime>();
+            if (weekEnds != null)
+            {
+                foreach (var day in weekEnds)
+                {
+                    DateTime currWeekEnd = day.StartDate;
+                    while (currWeekEnd <= day.EndDate)
+                    {
+                        weekEndList.Add(currWeekEnd);
+                        currWeekEnd = currWeekEnd.AddDays(1);
+                    }
+
+                }
+            }
+
+            var result = startDate;
+            var counter = 0;
+            while (counter < dayCount)
+            {
+                if (!weekEndList.Contains(result))
+                {
+                    counter += 1;
+                    if (counter == dayCount) break;
+                    result = result.AddDays(1);
+                }
+                else
+                {
+                    result = result.AddDays(1);
+                }
+            }
+
+            return result;
+        }
+    }
+}
